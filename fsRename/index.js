33b933123c09d93201b168543487e7628fc805e4
Nafi <work.nafi@gmail.com>
2021-09-08 11:29:04 +0600

    asynchronous rename

--- fsRename/index.js
+++ fsRename/index.js
@@ -4,37 +4,37 @@ const http = require('http');
 const server = http.createServer(function(req, res) {
 
     if (req.url == "/") {
-        //Asynchronous file write
-        // fs.writeFile('demo.txt', 'Hello world', function(error) {
+        //Asynchronous file Rename
+        fs.rename('demo.txt', 'Asynchronous.txt', function(error) {
 
-        //     if (error) {
-        //         res.writeHead(200, { 'content-Type': 'text/html' });
-        //         res.write("File Write failed");
-        //         res.end();
+            if (error) {
+                res.writeHead(200, { 'content-Type': 'text/html' });
+                res.write("File Rename failed");
+                res.end();
 
-        //     } else {
-        //         res.writeHead(200, { 'content-Type': 'text/html' });
-        //         res.write("File Write success");
-        //         res.end();
+            } else {
+                res.writeHead(200, { 'content-Type': 'text/html' });
+                res.write("File Rename success");
+                res.end();
 
-        //     }
+            }
 
-        // });
+        });
 
-        //synchronous file write
-        let error = fs.writeFileSync('synchronous.txt', 'welcome to synchronous write file')
+        //synchronous file rename
+        // let error = fs.writeFileSync('synchronous.txt', 'synchronousRename.txt', 'welcome to synchronous Rename file')
 
-        if (error) {
-            res.writeHead(200, { 'content-Type': 'text/html' });
-            res.write("File Write failed");
-            res.end();
+        // if (error) {
+        //     res.writeHead(200, { 'content-Type': 'text/html' });
+        //     res.write("File Rename failed");
+        //     res.end();
 
-        } else {
-            res.writeHead(200, { 'content-Type': 'text/html' });
-            res.write("File Write success");
-            res.end();
+        // } else {
+        //     res.writeHead(200, { 'content-Type': 'text/html' });
+        //     res.write("File Rename success");
+        //     res.end();
 
-        }
+        // }
 
     }
 
