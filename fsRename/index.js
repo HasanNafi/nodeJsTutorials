2ea83325becce52ac064fa1a37b6e589766624c6
Nafi <work.nafi@gmail.com>
2021-09-08 11:33:20 +0600

    synchronous rename

--- fsRename/index.js
+++ fsRename/index.js
@@ -5,36 +5,36 @@ const server = http.createServer(function(req, res) {
 
     if (req.url == "/") {
         //Asynchronous file Rename
-        fs.rename('demo.txt', 'Asynchronous.txt', function(error) {
+        // fs.rename('demo.txt', 'Asynchronous.txt', function(error) {
 
-            if (error) {
-                res.writeHead(200, { 'content-Type': 'text/html' });
-                res.write("File Rename failed");
-                res.end();
+        //     if (error) {
+        //         res.writeHead(200, { 'content-Type': 'text/html' });
+        //         res.write("File Rename failed");
+        //         res.end();
 
-            } else {
-                res.writeHead(200, { 'content-Type': 'text/html' });
-                res.write("File Rename success");
-                res.end();
+        //     } else {
+        //         res.writeHead(200, { 'content-Type': 'text/html' });
+        //         res.write("File Rename success");
+        //         res.end();
 
-            }
+        //     }
 
-        });
+        // });
 
-        //synchronous file rename
-        // let error = fs.writeFileSync('synchronous.txt', 'synchronousRename.txt', 'welcome to synchronous Rename file')
+        // synchronous file rename
+        let error = fs.renameSync('synchronous.txt', 'synchronousRename.txt')
 
-        // if (error) {
-        //     res.writeHead(200, { 'content-Type': 'text/html' });
-        //     res.write("File Rename failed");
-        //     res.end();
+        if (error) {
+            res.writeHead(200, { 'content-Type': 'text/html' });
+            res.write("File Rename failed");
+            res.end();
 
-        // } else {
-        //     res.writeHead(200, { 'content-Type': 'text/html' });
-        //     res.write("File Rename success");
-        //     res.end();
+        } else {
+            res.writeHead(200, { 'content-Type': 'text/html' });
+            res.write("File Rename success");
+            res.end();
 
-        // }
+        }
 
     }
 
