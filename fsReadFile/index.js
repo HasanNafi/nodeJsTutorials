4890a93174231a7198de87164b59492810284507
Nafi <work.nafi@gmail.com>
2021-09-08 09:35:48 +0600

    added read synchronous function

--- fsReadFile/index.js
+++ fsReadFile/index.js
@@ -2,20 +2,28 @@ const fs = require('fs');
 const http = require('http');
 
 const server = http.createServer(function(req, res) {
+    //asynchronous read
+    // if (req.url = "/") {
+    //     fs.readFile('home.html', function(err, data) {
 
+    //         res.writeHead(200, { 'content-Type': 'text/html' });
+    //         res.write(data);
+    //         res.end();
+
+    //     })
+
+
+    //synchronous read
     if (req.url = "/") {
-        fs.readFile('home.html', function(err, data) {
-
-            res.writeHead(200, { 'content-Type': 'text/html' });
-            res.write(data);
-            res.end();
-
-        })
+        let mydata = fs.readFileSync('home.html');
+        res.writeHead(200, { 'content-Type': 'text/html' });
+        res.write(mydata);
+        res.end();
 
     }
 
 
-})
+});
 
 server.listen(3000);
 console.log("server run success");
