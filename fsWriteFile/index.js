0901c1548565db33696cf07ef6243fb8cb1b97cc
Nafi <work.nafi@gmail.com>
2021-09-08 11:09:12 +0600

    added synchronous and asynchronous write file

--- fsWriteFile/index.js
+++ fsWriteFile/index.js
@@ -4,26 +4,41 @@ const http = require('http');
 const server = http.createServer(function(req, res) {
 
     if (req.url == "/") {
+        //Asynchronous file write
+        // fs.writeFile('demo.txt', 'Hello world', function(error) {
 
-        fs.writeFile('demo.txt', 'Hello world', function(error) {
+        //     if (error) {
+        //         res.writeHead(200, { 'content-Type': 'text/html' });
+        //         res.write("File Write failed");
+        //         res.end();
 
-            if (error) {
-                res.writeHead(200, { 'content-Type': 'text/html' });
-                res.write("File Write failed");
-                res.end();
+        //     } else {
+        //         res.writeHead(200, { 'content-Type': 'text/html' });
+        //         res.write("File Write success");
+        //         res.end();
 
-            } else {
-                res.writeHead(200, { 'content-Type': 'text/html' });
-                res.write("File Write success");
-                res.end();
+        //     }
 
-            }
+        // });
 
-        });
+        //synchronous file write
+        let error = fs.writeFileSync('synchronous.txt', 'welcome to synchronous write file')
+
+        if (error) {
+            res.writeHead(200, { 'content-Type': 'text/html' });
+            res.write("File Write failed");
+            res.end();
+
+        } else {
+            res.writeHead(200, { 'content-Type': 'text/html' });
+            res.write("File Write success");
+            res.end();
+
+        }
 
     }
 
 });
 
-server.listen(4000);
+server.listen(5000);
 console.log("server run success");
