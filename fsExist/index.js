35cb00b2d95758c8473f480205431154488e3582
Nafi <work.nafi@gmail.com>
2021-09-08 12:11:59 +0600

    synchronous and asychronous file search

---
+++ fsExist/index.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer(function(req, res) {
+
+    if (req.url == "/") {
+        // Asynchronous file exist
+        fs.exists('Asynchronous.txt', function(result) {
+
+            if (result) {
+
+                res.end("Asynchronous File found");
+
+            } else {
+
+                res.end("Asynchronous File not found");
+
+            }
+
+        });
+
+        //synchronous file delete
+        // let result = fs.existsSync('synchronousRename.txt')
+
+        // if (result) {
+
+        //     res.end("synchronous File found");
+
+        // } else {
+        //     res.end("Synchronous File not found");
+
+        // }
+
+    }
+
+});
+
+server.listen(5000);
+console.log("server run success");
